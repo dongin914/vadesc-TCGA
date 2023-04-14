c3c9ad465a950b48c704d1fac3e025aedabc1fff
dongin914@naver.com <dongin914@naver.com>
2023-04-14 10:07:04 +0900

    update-23.04.14

---
+++ train.py
@@ -0,0 +1,39 @@
+import os
+import random
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+import pickle
+
+from data_utils import DataGen, get_gen
+from model import GMM_Survival
+from losses import Losses
+from eval_utils_1 import cindex_metric
+from sklearn.model_selection import train_test_split
+
+def setup_seed(seed):
+    random.seed(seed)  
+    np.random.seed(seed) 
+    tf.random.set_seed(seed)
+    os.environ['TF_DETERMINISTIC_OPS'] = '1'  
+
+def train_model(configs, X_train, Y_train, X_test, Y_test, seed):
+    setup_seed(seed)
+
+    losses = Losses(configs)
+    rec_loss = losses.loss_reconstruction_mse
+
+    model = GMM_Survival(**configs['training'])
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.00001)
+    model.compile(optimizer, loss={"output_1": rec_loss},
+                  metrics={"output_2": cindex_metric})
+
+    tf.keras.backend.set_value(model.use_t, np.array([1.0]))
+
+    gen_train = get_gen(X_train, Y_train, configs, 16)
+    gen_test = get_gen(X_test, Y_test, configs, 16, validation=True)
+
+    model.fit(gen_train, validation_data=gen_test, epochs=160, verbose=1)
+    model.summary()
+
+    return model
